094108afc21525d7fff55eb73ab89187a619041c
Shashika Jayawardhana <shashika.janith@gmail.com>
2022-05-11 23:11:26 +0530

    Convert to typescript.

--- medi-bot-widget/src/index.tsx
+++ medi-bot-widget/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './assets/scss/animation.scss';
+import './assets/scss/main.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('chat-widget-root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
