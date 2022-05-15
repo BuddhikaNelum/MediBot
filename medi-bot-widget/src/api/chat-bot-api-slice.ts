a0b2d82e3c7c6d57919e9bca98606c00d8ba0e86
Shashika Janith <shashika.janith@gmail.com>
2022-05-16 01:10:08 +0530

    Update the API slice.

--- medi-bot-widget/src/api/chat-bot-api-slice.ts
+++ medi-bot-widget/src/api/chat-bot-api-slice.ts
@@ -7,8 +7,29 @@ export const apiSlice = createApi({
   endpoints: (build) => ({
     chat: build.query<ChatResponse, string>({
       query: (message) => `DialogFlow/DetectIntent/${message}`
-    })
+    }),
+    doctors: build.query<any, DoctorsRequest>({
+      query: (body) => ({
+        url: `DialogFlow/Doctors`,
+        method: 'POST',
+        body
+      })
+    }),
+    specialities: build.query<SpecialitiesResponse, SpecialitiesRequest>({
+      query: (body) => ({
+        url: `DialogFlow/Specialities`,
+        method: 'POST',
+        body
+      })
+    }),
+    booking: build.query<any, BookingRequest>({
+      query: (body) => ({
+        url: `DialogFlow/Booking`,
+        method: 'POST',
+        body
+      })
+    }),
   })
 })
 
-export const { useLazyChatQuery } = apiSlice;
+export const { useLazyChatQuery, useLazyDoctorsQuery, useLazySpecialitiesQuery, useLazyBookingQuery } = apiSlice;
