e43ac9ccf958a786f67310a62a94ffeb415e9a06
Shashika Janith <shashika.janith@gmail.com>
2022-05-15 10:05:21 +0530

    Add types

---
+++ medi-bot-widget/src/types/chat.type.d.ts
@@ -0,0 +1,9 @@
+import { Message, User } from "@progress/kendo-react-conversational-ui";
+
+type Chat = {
+  fulFillmentText: string;
+  intentName: string;
+  isIntentResponse: boolean;
+}
+
+type ChatResponse = Chat;
